be8def30a1c2c8bae512c92fd7dd72320e2b192e
mnn89ghub <info@massnonprofitnet.org>
2017-12-30 23:20:38 -0500

    started js :D

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-# TEST INDEX.JS
+// TEST INDEX.JS
 
+//button.onclick = function(){
+  //document.myFunction().style.color = "red";
+//}
+
+
+
+function myFunction2() {
+  document.getElementById("extern demo").style.color = "blue";
+}
